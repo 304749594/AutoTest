6b9cf9346fe1615f4e45411046b7e1fa24919709
wuxiaochen <304749594@qq.com>
2018-12-25 00:24:53 +0800

    201812250024

---
+++ Chapter10/src/main/java/com/course/bean/User.java
@@ -0,0 +1,8 @@
+package com.course.bean;
+
+/**
+ * Created by Administrator on 2018/12/24.
+ */
+
+public class User {
+}
---
+++ Chapter10/src/main/java/com/course/server/MyPostMethod.java
@@ -0,0 +1,8 @@
+package com.course.server;
+
+/**
+ * Created by Administrator on 2018/12/24.
+ */
+
+public class MyPostMethod {
+}
